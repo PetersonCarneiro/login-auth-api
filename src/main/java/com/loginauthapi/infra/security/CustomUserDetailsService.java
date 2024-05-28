14c4d2ccb524e27fad6eb76027ea8a7f835049c5
Peterson <peters.prestes@gmail.com>
2024-05-28 11:45:26 -0300

    Feat: Creation CustomUserDetailsService

---
+++ src/main/java/com/loginauthapi/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.loginauthapi.infra.security;
+
+import com.loginauthapi.domain.User;
+import com.loginauthapi.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User( user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
